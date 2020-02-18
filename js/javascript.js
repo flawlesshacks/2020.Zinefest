c43b9e41bc0c39d79478b05c67f937c21bb87481
Kevin Zheng <Kevin.Zheng4783@gmail.com>
2020-02-18 09:49:56 -0500

    Cleaned up slide code, added bootstrap grid sourcemap and edited bootstrap grid css to include local reference point of sourcemap

--- js/javascript.js
+++ js/javascript.js
@@ -1,64 +1,60 @@
-document.getElementById("zine-button-three").addEventListener("click", () => {
+const active_slide_one = (option1, option2) => {
   let slides_one = document.getElementById('zine-slide-one')
   let slides_two = document.getElementById('zine-slide-two')
-  let slides_three = document.getElementById('zine-slide-three')
-  let image_two = document.getElementById('zine-image-two')
-  let image_three = document.getElementById('zine-image-three')
-  slides_one.classList.add("transition-offscreen-2")
-  slides_two.classList.add("transition-offscreen-2")
-  slides_three.classList.add("transition-offscreen-3", "active")
-  image_two.classList.add("hidden")
-  image_three.classList.remove("hidden")
-});
+  let text_one = document.getElementById('zine-slide-one-text')
+  let text_two = document.getElementById('zine-slide-two-text')
+  slides_one.classList[option1]("transition-offscreen")
+  slides_two.classList[option1]("transition-offscreen")
+  text_one.classList[option1]("hidden")
+  text_two.classList[option2]("hidden")
+}
 
-document.getElementById("zine-button-two").addEventListener("click", () => {
+const active_slide_two = (option1, option2) => {
   let slides_one = document.getElementById('zine-slide-one')
   let slides_two = document.getElementById('zine-slide-two')
   let slides_three = document.getElementById('zine-slide-three')
   let text_one = document.getElementById('zine-slide-one-text')
   let text_two = document.getElementById('zine-slide-two-text')
+  slides_one.classList[option1]("transition-offscreen")
+  slides_two.classList[option1]("transition-offscreen")
+  slides_three.classList[option1]("transition-offscreen-2")
+  text_one.classList[option1]("hidden")
+  text_two.classList[option2]("hidden")
+}
+
+const active_slide_three = (option1, option2) => {
+  let slides_one = document.getElementById('zine-slide-one')
+  let slides_two = document.getElementById('zine-slide-two')
+  let slides_three = document.getElementById('zine-slide-three')
   let image_two = document.getElementById('zine-image-two')
   let image_three = document.getElementById('zine-image-three')
+  slides_one.classList[option1]("transition-offscreen-2")
+  slides_two.classList[option1]("transition-offscreen-2")
+  slides_three.classList[option1]("transition-offscreen-3", "active")
+  image_two.classList[option1]("hidden")
+  image_three.classList[option2]("hidden")
+}
+
+document.getElementById("zine-button-three").addEventListener("click", () => {
+  active_slide_three('add', 'remove')
+});
+
+document.getElementById("zine-button-two").addEventListener("click", () => {
+  let slides_three = document.getElementById('zine-slide-three')
   if (slides_three.classList.contains('active')) {
-    slides_one.classList.remove("transition-offscreen-2")
-    slides_two.classList.remove("transition-offscreen-2")
-    slides_three.classList.remove("transition-offscreen-3", "active")
-    image_two.classList.remove("hidden")
-    image_three.classList.add("hidden")
+    active_slide_three('remove', 'add')
+    active_slide_two('add', 'remove')
   } else {
-    slides_one.classList.add("transition-offscreen")
-    slides_two.classList.add("transition-offscreen")
-    slides_three.classList.add("transition-offscreen-2")
-    text_one.classList.add("hidden")
-    text_two.classList.remove("hidden")
+    active_slide_two('add', 'remove')
   }
 });
 
 document.getElementById("zine-button-one").addEventListener("click", () => {
-  let slides_one = document.getElementById('zine-slide-one')
-  let slides_two = document.getElementById('zine-slide-two')
   let slides_three = document.getElementById('zine-slide-three')
-  let text_one = document.getElementById('zine-slide-one-text')
-  let text_two = document.getElementById('zine-slide-two-text')
-  let image_two = document.getElementById('zine-image-two')
-  let image_three = document.getElementById('zine-image-three')
   if (slides_three.classList.contains('active')) {
-    slides_one.classList.remove("transition-offscreen")
-    slides_two.classList.remove("transition-offscreen")
-    slides_three.classList.remove("transition-offscreen-2")
-    slides_one.classList.remove("transition-offscreen-2")
-    slides_two.classList.remove("transition-offscreen-2")
-    slides_three.classList.remove("transition-offscreen-3", "active")
-    text_one.classList.remove("hidden")
-    text_two.classList.add("hidden")
-    image_two.classList.remove("hidden")
-    image_three.classList.add("hidden")
+    active_slide_two('remove', 'add')
+    active_slide_three('remove', 'add')
   } else {
-    let text_one = document.getElementById('zine-slide-one-text')
-    let text_two = document.getElementById('zine-slide-two-text')
-    slides_one.classList.remove("transition-offscreen")
-    slides_two.classList.remove("transition-offscreen")
-    text_one.classList.remove("hidden")
-    text_two.classList.add("hidden")
+    active_slide_one('remove', 'add')
   }
 });
