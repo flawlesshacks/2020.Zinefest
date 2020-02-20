a03c8070b9e483963b4ddc5a95a5ac8e1fb9c8bd
Kevin Zheng <Kevin.Zheng4783@gmail.com>
2020-02-20 05:59:12 -0500

    Adjusted Zine colors and fixed click handler on arrow controls

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,10 @@
 // ===== ZINE SECTION
 // ==== Functions
-// === Toggles 'Active' class on slides
+// === Toggles "Active" class on slides
 const isActive = (slide) => {
   const slides_one = document.getElementById(`zine-slide-one`)
-  const slides_two = document.getElementById('zine-slide-two')
-  const slides_three = document.getElementById('zine-slide-three')
+  const slides_two = document.getElementById("zine-slide-two")
+  const slides_three = document.getElementById("zine-slide-three")
   if (slide === "zine-slide-one") {
     slides_one.classList.add("active")
     slides_two.classList.remove("active")
@@ -24,10 +24,10 @@ const isActive = (slide) => {
 
 // === Toggles "add" or "remove" on slide one
 const toggle_slide_one = (option1, option2) => {
-  const slides_one = document.getElementById('zine-slide-one')
-  const slides_two = document.getElementById('zine-slide-two')
-  const text_one = document.getElementById('zine-slide-one-text')
-  const text_two = document.getElementById('zine-slide-two-text')
+  const slides_one = document.getElementById("zine-slide-one")
+  const slides_two = document.getElementById("zine-slide-two")
+  const text_one = document.getElementById("zine-slide-one-text")
+  const text_two = document.getElementById("zine-slide-two-text")
   slides_one.classList[option1]("transition-offscreen")
   slides_two.classList[option1]("transition-offscreen")
   text_one.classList[option1]("hidden")
@@ -36,11 +36,11 @@ const toggle_slide_one = (option1, option2) => {
 
 // === Toggles "add" or "remove" on slide two
 const toggle_slide_two = (option1, option2) => {
-  const slides_one = document.getElementById('zine-slide-one')
-  const slides_two = document.getElementById('zine-slide-two')
-  const slides_three = document.getElementById('zine-slide-three')
-  const text_one = document.getElementById('zine-slide-one-text')
-  const text_two = document.getElementById('zine-slide-two-text')
+  const slides_one = document.getElementById("zine-slide-one")
+  const slides_two = document.getElementById("zine-slide-two")
+  const slides_three = document.getElementById("zine-slide-three")
+  const text_one = document.getElementById("zine-slide-one-text")
+  const text_two = document.getElementById("zine-slide-two-text")
   slides_one.classList[option1]("transition-offscreen")
   slides_two.classList[option1]("transition-offscreen")
   slides_three.classList[option1]("transition-offscreen-2")
@@ -50,11 +50,11 @@ const toggle_slide_two = (option1, option2) => {
 
 // === Toggles "add" or "remove" on slide three
 const toggle_slide_three = (option1, option2) => {
-  const slides_one = document.getElementById('zine-slide-one')
-  const slides_two = document.getElementById('zine-slide-two')
-  const slides_three = document.getElementById('zine-slide-three')
-  const image_two = document.getElementById('zine-image-two')
-  const image_three = document.getElementById('zine-image-three')
+  const slides_one = document.getElementById("zine-slide-one")
+  const slides_two = document.getElementById("zine-slide-two")
+  const slides_three = document.getElementById("zine-slide-three")
+  const image_two = document.getElementById("zine-image-two")
+  const image_three = document.getElementById("zine-image-three")
   slides_one.classList[option1]("transition-offscreen-2")
   slides_two.classList[option1]("transition-offscreen-2")
   slides_three.classList[option1]("transition-offscreen-3")
@@ -65,59 +65,60 @@ const toggle_slide_three = (option1, option2) => {
 // === Update Arrow
 const updateArrow = (arrow, option) => {
   const arrowEle = document.getElementById(arrow)
-  arrowEle.classList[option]('arrow-disabled')
+  arrowEle.classList[option]("arrow-disabled")
 }
 
 // === Change to slide one
 const handleButtonOne = () => {
-  const slides_three = document.getElementById('zine-slide-three')
-  if (slides_three.classList.contains('active')) {
-    toggle_slide_two('remove', 'add')
-    toggle_slide_three('remove', 'add')
+  const slides_three = document.getElementById("zine-slide-three")
+  if (slides_three.classList.contains("active")) {
+    toggle_slide_two("remove", "add")
+    toggle_slide_three("remove", "add")
   } else {
-    toggle_slide_one('remove', 'add')
+    toggle_slide_one("remove", "add")
   }
   isActive("zine-slide-one")
-  updateArrow('arrow-left', 'add')
-  updateArrow('arrow-right', 'remove')
+  updateArrow("arrow-left", "add")
+  updateArrow("arrow-right", "remove")
 }
 
 // === Change to slide two
 const handleButtonTwo = () => {
-  const slides_three = document.getElementById('zine-slide-three')
-  if (slides_three.classList.contains('active')) {
-    toggle_slide_three('remove', 'add')
-    toggle_slide_two('add', 'remove')
+  const slides_three = document.getElementById("zine-slide-three")
+  if (slides_three.classList.contains("active")) {
+    toggle_slide_three("remove", "add")
+    toggle_slide_two("add", "remove")
   } else {
-    toggle_slide_two('add', 'remove')
+    toggle_slide_two("add", "remove")
   }
   isActive("zine-slide-two")
-  updateArrow('arrow-left', 'remove')
-  updateArrow('arrow-right', 'remove')
+  updateArrow("arrow-left", "remove")
+  updateArrow("arrow-right", "remove")
 }
 
 // === Change to slide three
 const handleButtonThree = () => {
-  toggle_slide_three('add', 'remove')
+  toggle_slide_three("add", "remove")
   isActive("zine-slide-three")
-  updateArrow('arrow-left', 'remove')
-  updateArrow('arrow-right', 'add')
+  updateArrow("arrow-left", "remove")
+  updateArrow("arrow-right", "add")
 }
 
 // === Changes between slides depending on current active slide
 handleArrow = (arrow) => {
-  const slides_two = document.getElementById('zine-slide-two')
-  const slides_three = document.getElementById('zine-slide-three')
-  if (slides_three.classList.contains('active')) {
+  const slides_one = document.getElementById("zine-slide-one")
+  const slides_two = document.getElementById("zine-slide-two")
+  const slides_three = document.getElementById("zine-slide-three")
+  if (slides_three.classList.contains("active") && arrow !== "arrow-right") {
     handleButtonTwo()
-  } else if (arrow === 'arrow-left') {
-    if (slides_two.classList.contains('active')) {
+  } else if (arrow === "arrow-left") {
+    if (slides_two.classList.contains("active")) {
       handleButtonOne()
     }
   } else {
-    if (slides_two.classList.contains('active')) {
+    if (slides_two.classList.contains("active")) {
       handleButtonThree()
-    } else {
+    } else if(slides_one.classList.contains("active")) {
       handleButtonTwo()
     }
   }
